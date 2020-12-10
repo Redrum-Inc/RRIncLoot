812a9d6b90610d6a8480ee83f9380bfc42bd8a54
unknown <botolfs@gmail.com>
2020-12-10 19:49:08 +0100

    added TRASHDATA.lua

---
+++ TRASHDATA.lua
@@ -0,0 +1,58 @@
+TrashData = {
+    Items = {
+        
+        { -- Common items
+            "Linen Cloth",
+            "Runecloth",
+            "Ironweb Spider Silk"
+        },
+        { -- Ahn'Qiraj
+            "Greater Scarab Coffer Key",
+            "Gold Scarab",
+            "Silver Scarab",
+            "Stone Scarab",
+            "Bone Scarab",
+            "Bronze Scarab",
+            "Crystal Scarab",
+            "Clay Scarab",
+            "Ivory Scarab",
+            "Idol of the Sun",
+            "Idol of Strife",
+            "Idol of the Sage",
+            "Idol of Death",
+            "Idol of Life",
+            "Idol of Night",
+            "Idol of Rebirth",
+            "Idol of War"
+        },
+        { -- Naxxramas
+            "Word of Thawing",
+            "Wartorn Cloth Scrap",
+            "Wartorn Leather Scrap",
+            "Wartorn Plate Scrap",
+            "Wartorn Chain Scrap"            
+        },
+        { -- Test, lvl 5-6 defias outside stormwind
+            "Malachite",
+            "Refreshing Spring Water",
+            "Linen Cloth",
+            "Darnassian Bleu",
+            "Scroll of Protection",
+            "Flimsy Chain Cloak",
+            "Ragged Leather Vest",
+            "Ragged Leather Gloves",
+            "Flimsy Chain Boots",
+            "Worn Leather Belt",
+            "Patchwork Shoes",
+            "Ragged Leather Pants",
+            "Ragged Leather Boots",
+            "Flimsy Chain Bracers",
+            "Worn Hide Cloak"
+        }
+    }
+    -- Test = 
+    -- BWL = 
+    -- AQ = ,
+    -- Naxx = 
+}
+
