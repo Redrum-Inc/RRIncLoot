7936335e4f9f6612a0be5cca5277f4a15bf1c5e1
bo12s <botolfs@gmail.com>
2020-12-11 12:19:54 +0100

    added RRIncLoot_PlayerIsMasterlooter to shared

--- RRIncLoot_Shared.lua
+++ RRIncLoot_Shared.lua
@@ -8,3 +8,18 @@ function RRIncLoot_LootCanBeAutolooted(item)
 	end
 	return false
 end
+
+function RRIncLoot_PlayerIsMasterlooter()
+	local masterlooterRaidID = select(3, GetLootMethod())
+	if masterlooterRaidID == nil then
+		-- masterlooterRaidID - Returns index of the master looter in the raid (corresponding to a raidX unit), or nil if the player is not in a raid or master looting is not used.
+		return false
+	else 
+		local rosterName = select(1, GetRaidRosterInfo(masterlooterRaidID)) 
+		local playerName = select(1, UnitName("player"))
+		if rosterName ~= playerName then			
+			return false
+		end
+	end	
+	return true
+end
