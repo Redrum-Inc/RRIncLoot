ba596a8495126e358060c0e09d644c90e6fe89d6
unknown <botolfs@gmail.com>
2021-01-15 16:45:13 +0100

    removed obsolete console commands and clear history functions

--- RRIncLoot_History.lua
+++ RRIncLoot_History.lua
@@ -1,7 +1,5 @@
 -- Loot distribution History
 
-LootHistory = {}
-
 function RRIncLoot_AddLootHistory(player, action, item)
 	-- print(player, action, item)
 	local timestampformat="%y-%m-%d %H:%M:%S";
@@ -12,33 +10,4 @@ end
 function ClearLootHistory()
 	LootHistory = {}
 	print("LootHistory cleared.")
-end
-
-SLASH_RRINCLOOTHISTORY1 = '/loothistory'
-SLASH_RRINCLOOTHISTORY1 = '/lh'
-function SlashCmdList.RRINCLOOTHISTORY(msg)
-	-- Fix manual controls of adding/changing/removing history
-	if(msg == "") then
-		print("Passed/Lost:")
-		for i = 1, #LootHistory do
-			if string.find(LootHistory[i], "passed on") or string.find(LootHistory[i], "lost roll for") then
-				print(LootHistory[i])
-			end
-		end
-		print("----------")
-		print("Accepted/Won:")
-		for i = 1, #LootHistory do
-			if string.find(LootHistory[i], "accepted") or string.find(LootHistory[i], "won roll for") then
-				print(LootHistory[i])
-			end
-		end
-		print("----------")
-	end
-
-	if(msg == "clear") then
-		ClearLootHistory()
-	end
-
-	if(msg == "add") then
-	end
 end
